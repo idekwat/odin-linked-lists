128983472909b720b0f48d31e4bc789168d8a826
idekwat <184945414+idekwat@users.noreply.github.com>
2025-07-28 22:10:42 +0800

    added starting js file for linkedlist

---
+++ linkedlist.js
@@ -0,0 +1,39 @@
+export default class LinkedList {
+    append(value) {
+
+    }
+    prepend(value) {
+
+    }
+    size() {
+
+    }
+    head() {
+
+    }
+    tail() {
+
+    }
+    at(index) {
+        
+    }
+    pop() {
+
+    }
+    contains(value) {
+
+    }
+    find(value) {
+
+    }
+    toString() {
+        return ("( " + value + " )" +  " -> " + "( " + value + " )" + " -> " + "null");
+    }
+}
+
+class Node {
+    constructor(value, nextNode) {
+        this.value = null;
+        this.nextNode = null;
+    }
+}
